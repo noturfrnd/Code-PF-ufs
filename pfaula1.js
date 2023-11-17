1a1bd8ce04df4d1b1c054a9788e59e6746fcfa2c
noturfrnd <jpaneto@dcomp.ufs.br>
2023-11-17 01:35:56 -0300

    PF files 1

---
+++ pfaula1.js
@@ -0,0 +1,13 @@
+const roletaRussa = ["Pague 10 reais",
+"Pague uma coca",
+"10 flexoes",
+"Hello World , neutro",
+"10 agachamentos",
+"Beba agua",
+"Cochilo sem despertador",
+"Fazer live NPC",
+"🎉 Parabens vc ganhou!",]
+
+const rand = Math.floor(Math.random() * roletaRussa.length)
+
+console.log(roletaRussa[rand]);
