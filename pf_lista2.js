632992ea033e85baba4414d9e4a29b24a5ba3dd1
noturfrnd <jpaneto@dcomp.ufs.br>
2023-11-23 20:07:56 -0300

    lista 2 q2 a q4

---
+++ pf_lista2.js
@@ -0,0 +1,42 @@
+// Pureza!
+
+// Questão 2
+function areaCirculo (raio){
+    const pi = 3.14;
+    return pi * raio * raio;
+}
+
+console.log(`Area do circulo com raio 5: ${areaCirculo(5)}`);
+console.log(`Area do circulo com raio 10: ${areaCirculo(10)}`);
+console.log(`Area do circulo com raio 3.14: ${areaCirculo(3.14)}`);
+
+// Questão 3 usando propriedade da desigualdade traingular onde a + b > c;
+function fMedidasTriang(ladoA, ladoB, ladoC){
+    if(ladoA + ladoB > ladoC){
+        console.table({'Lado A':ladoA, 'Lado B': ladoB, 'Lado C': ladoC, 'Forma Triangulo': true});
+        return true; 
+    }
+    console.table({'Lado A':ladoA, 'Lado B': ladoB, 'Lado C': ladoC, 'Forma Triangulo': false});
+    return false; 
+}
+
+console.log(`Forma triangulo? ${fMedidasTriang(4,4,7)}`);
+console.log(`Forma triangulo? ${fMedidasTriang(4,3,12)}`);
+console.log(`Forma triangulo? ${fMedidasTriang(8,4,11)}`);
+
+// Questao 4
+function classifyTriangle(ladoA, ladoB, ladoC){
+    if (ladoA == ladoB && ladoB == ladoC){
+        return "Triangulo equilatero";
+    }
+    else if (ladoA != ladoB && ladoB != ladoC && ladoC != ladoA){
+        return "Triangulo Escaleno";
+    }
+    else {
+        return "Triangulo isoceles";
+    }
+}
+
+console.log(`Tipo de triangulo: ${classifyTriangle(2,2,3)}`);
+console.log(`Tipo de triangulo: ${classifyTriangle(2,2,2)}`);
+console.log(`Tipo de triangulo: ${classifyTriangle(2,8,3)}`);
